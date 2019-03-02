a62857d36af29de1756992fdd4695a0139ec758a
bhavik103 <bhavikkalariya103@gmail.com>
2019-03-02 17:08:01 +0530

    material custom template

--- js/script.js
+++ js/script.js
@@ -11,4 +11,15 @@ $(document).ready(function(){
 		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
 		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
 	});
+
+	$('.mainSliderTwo').slick({
+		arrows: true,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+	});
 });
+
+
